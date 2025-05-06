f752e60ebf95e5544c62b1d11061e6629dea5278
Mahammud Hassan Limon <156072234+Limon-Hassan@users.noreply.github.com>
2025-05-06 13:17:04 +0600

    efefsf

--- main.js
+++ main.js
@@ -25,6 +25,6 @@ mongoose.connect(process.env.DB_url).then(() => {
 app.get('/', (req, res) => {
   res.send('server is run');
 });
-app.listen(process.env.local_host || 4500, () => {
+app.listen(process.env.SARVER_PORT || 4500, () => {
   console.log('Server is running');
 });
